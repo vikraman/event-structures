4a48c7a0ffbccb5bd04c3fc88595a23e2c1ce1d7
Vikraman Choudhury <git@vikraman.org>
2025-11-30 17:15:39 +0000

    feat: prove trace equivalence is a congruence

--- EventStructures/Trace.lean
+++ EventStructures/Trace.lean
@@ -49,4 +49,41 @@ instance : Equivalence (TraceEquiv es) where
   symm := @traceEquiv_symm es
   trans := @traceEquiv_trans es
 
+/-- Trans instance for calc proofs. -/
+instance : Trans (TraceEquiv es) (TraceEquiv es) (TraceEquiv es) where
+  trans h₁₂ h₂₃ := traceEquiv_trans es h₁₂ h₂₃
+
+/-- Trace equivalence is a left congruence for append. -/
+lemma traceEquiv_append_left {t₁ t₂ : List es.Event} (h : t₁ ≈ₜ t₂) (t : List es.Event) :
+    (t ++ t₁) ≈ₜ (t ++ t₂) := by
+  induction h generalizing t with
+  | refl t => exact TraceEquiv.refl _
+  | @swap e₁ e₂ t₁' t₂' t₃' ind h ih =>
+    -- h: (t₁' ++ e₁ :: e₂ :: t₂') ≈ₜ t₃'
+    -- Goal: (t ++ t₁' ++ e₂ :: e₁ :: t₂') ≈ₜ (t ++ t₃')
+    calc (t ++ (t₁' ++ e₂ :: e₁ :: t₂'))
+        = ((t ++ t₁') ++ e₂ :: e₁ :: t₂') := by simp [List.append_assoc]
+      _ ≈ₜ ((t ++ t₁') ++ e₁ :: e₂ :: t₂') := TraceEquiv.swap ind (TraceEquiv.refl _)
+      _ = (t ++ (t₁' ++ e₁ :: e₂ :: t₂')) := by simp [List.append_assoc]
+      _ ≈ₜ (t ++ t₃') := ih t
+
+/-- Trace equivalence is a right congruence for append. -/
+lemma traceEquiv_append_right {t₁ t₂ : List es.Event} (h : t₁ ≈ₜ t₂) (t : List es.Event) :
+    (t₁ ++ t) ≈ₜ (t₂ ++ t) := by
+  induction h with
+  | refl t => exact TraceEquiv.refl _
+  | @swap e₁ e₂ t₁' t₂' t₃' ind h ih =>
+    -- h: (t₁' ++ e₁ :: e₂ :: t₂') ≈ₜ t₃'
+    -- Goal: (t₁' ++ e₂ :: e₁ :: t₂' ++ t) ≈ₜ (t₃' ++ t)
+    calc (t₁' ++ e₂ :: e₁ :: t₂' ++ t)
+        = (t₁' ++ e₂ :: e₁ :: (t₂' ++ t)) := by simp [List.append_assoc]
+      _ ≈ₜ (t₁' ++ e₁ :: e₂ :: (t₂' ++ t)) := TraceEquiv.swap ind (TraceEquiv.refl _)
+      _ = (t₁' ++ e₁ :: e₂ :: t₂' ++ t) := by simp [List.append_assoc]
+      _ ≈ₜ (t₃' ++ t) := ih
+
+/-- Trace equivalence is a congruence for append. -/
+lemma traceEquiv_append {t₁ t₂ t₃ t₄ : List es.Event}
+    (h₁ : t₁ ≈ₜ t₂) (h₂ : t₃ ≈ₜ t₄) : (t₁ ++ t₃) ≈ₜ (t₂ ++ t₄) :=
+  traceEquiv_trans es (traceEquiv_append_right es h₁ t₃) (traceEquiv_append_left es h₂ t₂)
+
 end Trace
